ef00279803b710e6b6551677d94009555ae7a9b3
Unknown <ugo.schiapparelli@gmail.com>
2018-02-05 12:47:21 +0100

    First Commit

---
+++ OedivXuej/Source/OedivXuej/OedivXuejCharacter.cpp
@@ -0,0 +1,180 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "OedivXuejCharacter.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+
+DEFINE_LOG_CATEGORY(LogMyGame);
+
+//////////////////////////////////////////////////////////////////////////
+// AOedivXuejCharacter
+
+AOedivXuejCharacter::AOedivXuejCharacter()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Create"));
+
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
+	// set our turn rates for input
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	// Create a follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+	firstRight = true;
+	firstForward = true;
+
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Input
+
+void AOedivXuejCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// Set up gameplay key bindings
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AOedivXuejCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AOedivXuejCharacter::MoveRight);
+
+	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+	// "turn" handles devices that provide an absolute delta, such as a mouse.
+	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AOedivXuejCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AOedivXuejCharacter::LookUpAtRate);
+
+	// handle touch devices
+	PlayerInputComponent->BindTouch(IE_Pressed, this, &AOedivXuejCharacter::TouchStarted);
+	PlayerInputComponent->BindTouch(IE_Released, this, &AOedivXuejCharacter::TouchStopped);
+
+	// VR headset functionality
+	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AOedivXuejCharacter::OnResetVR);
+}
+
+
+void AOedivXuejCharacter::OnResetVR()
+{
+	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+}
+
+void AOedivXuejCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
+{
+		Jump();
+}
+
+void AOedivXuejCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
+{
+		StopJumping();
+}
+
+void AOedivXuejCharacter::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AOedivXuejCharacter::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AOedivXuejCharacter::MoveForward(float Value)
+{
+	FVector newLocation = this->GetActorLocation();
+
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		if (firstForward)
+		{
+			firstForward = false;
+		}
+		else if (precForward != newLocation)
+		{
+			FVector test = newLocation - precForward;
+
+			if (test.X * GetPendingMovementInputVector().X < 0 && test.Y * GetPendingMovementInputVector().Y < 0) {
+				UE_LOG(LogMyGame, Warning, TEXT("%s, %s"), *GetPendingMovementInputVector().ToString(), *test.ToString());
+				UE_LOG(LogMyGame, Warning, TEXT("Not Pass"));
+			}
+		}
+
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+
+	precForward = this->GetActorLocation();
+}
+
+void AOedivXuejCharacter::MoveRight(float Value)
+{
+	FVector newLocation = this->GetActorLocation();
+
+	if ( (Controller != NULL) && (Value != 0.0f) )
+	{
+
+		if (firstRight)
+		{
+			firstRight = false;
+		}
+		else if (precRight != newLocation)
+		{
+			FVector test = newLocation - precRight;
+
+			if (test.X * GetPendingMovementInputVector().X < 0 && test.Y * GetPendingMovementInputVector().Y < 0) {
+				UE_LOG(LogMyGame, Warning, TEXT("%s, %s"), *GetPendingMovementInputVector().ToString(), *test.ToString());
+				UE_LOG(LogMyGame, Warning, TEXT("Not Pass"));
+			}
+		}
+
+		// find out which way is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get right vector 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
+		AddMovementInput(Direction, Value);
+
+	}
+
+	precRight = this->GetActorLocation();
+
+}
---
+++ OedivXuej/Source/OedivXuej/OedivXuejGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OedivXuejGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOedivXuejGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOedivXuejGameMode();
+};
+
+
+
