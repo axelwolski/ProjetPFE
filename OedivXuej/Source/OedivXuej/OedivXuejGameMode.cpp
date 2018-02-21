b0032a5eb4937032404c47dd9b844260cc90f66f
shinhra <lecther-one@hotmail.fr>
2018-02-21 16:31:53 +0100

    ajout de la porte pour le lobby

--- OedivXuej/Source/OedivXuej/OedivXuejGameMode.cpp
+++ OedivXuej/Source/OedivXuej/OedivXuejGameMode.cpp
@@ -13,3 +13,30 @@ AOedivXuejGameMode::AOedivXuejGameMode()
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
+
+
+void AOedivXuejGameMode::PostLogin(APlayerController * NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+	TArray<AActor*> ActorList = GetWorld()->GetCurrentLevel()->Actors;
+	UNetworkGameInstance* Gi = Cast<UNetworkGameInstance>(GetWorld()->GetGameInstance());
+	if(Gi)
+	{
+		if(!Gi->bMultiPlayerGame || GetNumPlayers() > 1)
+		{
+			if(GetWorld()->GetMapName().Mid(GetWorld()->StreamingLevelsPrefix.Len()) == "Arena_V1")
+			{
+				for (int32 i = 0; i != ActorList.Num(); i++)
+				{
+					if(ActorList[i]->GetActorLabel() == "ArenaV1_Lobby_Door_BP")
+					{
+						if(ActorList[i]->Destroy())
+						{
+							ActorList.RemoveAt(i);
+						}
+					}
+				}
+			}
+		}
+	}
+}
