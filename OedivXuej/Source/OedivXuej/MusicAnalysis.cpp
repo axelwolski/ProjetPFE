6f7ac44a53604a141d7fff33670beedb14900e8a
shinhra <lecther-one@hotmail.fr>
2018-03-21 18:29:46 +0100

    Amélioration de la fonction getbpm

--- OedivXuej/Source/OedivXuej/MusicAnalysis.cpp
+++ OedivXuej/Source/OedivXuej/MusicAnalysis.cpp
@@ -5,24 +5,25 @@
 
 float UMusicAnalysis::GetBpm(FString SourceName)
 {
-	uint_t HopSize = 1024;
 	uint_t WinSize = 1024;
-	uint_t Framerate = 44100;
+	uint_t HopSize = 1024;
+	uint_t Samplerate = 44100;
 	uint_t Read = 0;
 
 	FString SourcePath = FPaths::ProjectDir() + "Content/Music/" + SourceName;
 	//GEngine->AddOnScreenDebugMessage(-1, 120.f, FColor::Red, FPaths::ProjectDir());
 	char_t* Source_path = TCHAR_TO_ANSI(*SourcePath);
 
-	aubio_source_t* Source = new_aubio_source(Source_path, Framerate, HopSize);
+	aubio_source_t* Source = new_aubio_source(Source_path, Samplerate, HopSize);
 	if (!Source)
 		return 0.f;
 
 	fvec_t* In = new_fvec(HopSize); // input audio buffer
 	fvec_t* Out = new_fvec(1); // output position
 
-	aubio_tempo_t * Tempo = new_aubio_tempo("default", WinSize, HopSize, Framerate);
-	uint_t BpmMean = 0;
+	aubio_tempo_t * Tempo = new_aubio_tempo("default", WinSize, HopSize, Samplerate);
+	float BpmMean = 0.f;
+	float Frames = 0.f;
 	do
 	{
 		aubio_source_do(Source, In, &Read);
@@ -30,9 +31,16 @@ float UMusicAnalysis::GetBpm(FString SourceName)
 
 		if (Out->data[0] != 0)
 		{
-			return aubio_tempo_get_bpm(Tempo);
+			BpmMean += aubio_tempo_get_bpm(Tempo);
+			Frames ++;
 		}
+
 	} while (Read == HopSize);
 
-	return 0.f;
+	del_aubio_tempo(Tempo);
+  del_fvec(In);
+  del_fvec(Out);
+  del_aubio_source(Source);
+
+	return BpmMean/Frames;
 }
